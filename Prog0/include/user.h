8df2f13489a2ef704e2d0a948d69f4d0e609a8aa
HydroAcrid <kevindotel21@gmail.com>
2023-09-07 16:39:46 -0400

    Created new_pass function

--- Prog0/include/user.h
+++ Prog0/include/user.h
@@ -14,4 +14,4 @@ typedef struct User user_t;
 void new_password(user_t *u);
 int read_users(user_t *user_list, char* filename);
 int save_users(user_t *user_list, char*filename);
-int find_user(user_t *user_list, char* username, char* password=NULL, int size);
+int find_user(user_t *user_list, char* username, char* password, int size);
--- Prog0/src/user.c
+++ Prog0/src/user.c
@@ -3,3 +3,20 @@
 #include <string.h>
 #include <time.h>
 #include "user.h"
+
+void new_password(user_t *u) {
+    //Generate a random number between 8 to 10 characters
+    srand(time(NULL));
+
+    //Characters that can be in the password
+    const char characters[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-=_+";
+    int characterLength = sizeof(characters) - 1;
+
+    //Between 8 and 10. %3 awlays returns from 0 - 2 
+    int passLength = rand() % 3 + 8; 
+
+    //Adds the characters into the password once the length is decided 
+    for(int i=0; i < passLength; i++) {
+        u -> password[i] = characters[rand() % characterLength];
+    }
+}
