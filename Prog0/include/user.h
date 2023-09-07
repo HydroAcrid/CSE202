2f97a1bdbf9b79e9e161fb7c1d9a16a6be89cbe8
HydroAcrid <kevindotel21@gmail.com>
2023-09-07 09:13:33 -0400

    added users.txt and made the header file

---
+++ Prog0/include/user.h
@@ -0,0 +1,17 @@
+typedef enum {
+  USER,
+  ADMIN
+} ACCESS;
+
+struct User {
+    char* username;
+    char* password;
+    enum ACCESS privilege; 
+};
+
+typedef struct User user_t; 
+
+void new_password(user_t *u);
+int read_users(user_t *user_list, char* filename);
+int save_users(user_t *user_list, char*filename);
+int find_user(user_t *user_list, char* username, char* password=NULL, int size);
