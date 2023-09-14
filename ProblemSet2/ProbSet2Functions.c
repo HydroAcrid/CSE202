1e4f57ec92957405ec97bcb669fbcde99f082fd9
HydroAcrid <kevindotel21@gmail.com>
2023-09-14 00:08:15 -0400

    made the function for prob

--- ProblemSet2/ProbSet2Functions.c
+++ ProblemSet2/ProbSet2Functions.c
@@ -6,11 +6,29 @@
 * For example, 0xFF00 -> 0x0100, and 0x66C0 -> 0x0040
 * If x=0, then return 0.
 */
-int rightmost_one(unsigned x);
+int rightmost_one(unsigned x) {
+    //Mask variable setup
+    unsigned mask = 1;
+    //Makes sure its not zero 
+    if(x == 0) {
+        return 0;
+    }
+
+    //Goes through every bit of x and checks x against the mask until the "AND" operator returns true 
+    for(int i = 0; i < 32; i++) {
+        if(x & mask) {
+            return mask;
+        }
+        mask <<= 1;
+    }
+
+    return 0; 
+}
 
 
 
 int main() {
     int num = (int)2147483648U;
-    printf("This is it: %d \n", num);
+    int maskCheck = rightmost_one(80);
+    printf("This is it: %d \n", maskCheck);
 }
