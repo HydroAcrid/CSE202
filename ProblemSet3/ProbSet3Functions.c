cd5349381ae55de6e4b8faa33b53126d9c98e35e
HydroAcrid <kevindotel21@gmail.com>
2023-09-20 08:12:31 -0400

    edited a little bit

--- ProblemSet3/ProbSet3Functions.c
+++ ProblemSet3/ProbSet3Functions.c
@@ -3,45 +3,46 @@
 
 /* compute 2*f. If f is NaN or f is Infinity, then return f */
 unsigned float_twice(unsigned f) {
-    // Extract sign, exponent, and fraction
-    unsigned sign = f & 0x80000000;
+    //Extract sign, exponent, and fraction
+    unsigned sign = f & 0x80000000; //isolates the sign bit 
     unsigned exp = (f >> 23) & 0xFF;
     unsigned frac = f & 0x007FFFFF;
 
-    // If f is NaN or Infinity, return f
+    //If f is NaN or Infinity, return f
     if (exp == 0xFF) {
         return f;
     }
 
-    // If f is denormalized
+    //If f is denormalized
     if (exp == 0) {
-        // Shift the fraction to the left
+        //Shift the fraction to the left
         frac <<= 1;
-        // If the fraction overflows (i.e., bit 23 becomes 1)
+        //If the fraction overflows 
         if (frac & 0x00800000) {
-            // Clear the overflow bit and increment the exponent
+            //Clear the overflow bit and increment the exponent
             frac &= 0x007FFFFF;
             exp = 1;
         }
     }
-    // If f is normalized
+    
+    //If f is normalized
     else {
-        // Increment the exponent
+        //Increment the exponent
         exp++;
-        // If the exponent becomes 255 (Infinity)
+        //If the exponent becomes 255 (Infinity)
         if (exp == 0xFF) {
-            // Set fraction to 0 (for Infinity)
+            //Set fraction to 0 (for Infinity)
             frac = 0;
         }
     }
 
-    // Reassemble the bits
-    return sign | (exp << 23) | frac;
+    //Reassemble the bits
+    return sign | (exp << 23) | frac; //use or statements to combine 
 }
 
 
 int main() {
-     unsigned f1 = 0x40C80000; // Representing 6.0 in IEEE 754
+    unsigned f1 = 0x40C80000; // Representing 6.0 in IEEE 754
     unsigned f2 = 0x7F800000; // Representing Infinity
     unsigned f3 = 0x00000001; // Smallest denormalized number
 
